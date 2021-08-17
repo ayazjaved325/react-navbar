bfc3db1dcad6662eff2556f904eca35d797fda6d
Ayaz Javed <ayazjaved325@gmail.com>
2021-08-17 18:32:54 +0500

    changes in app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import Home from './pages';
+import Home from './pages/home';
 import About from './pages/about';
+import Services from './pages/services';
+import ContactUs from './pages/contactus';
+import SignUp from './pages/signup';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,8 +13,11 @@ function App() {
     <Router>
     <Navbar/>
     <Switch>
-    <Route path="/" exact component={Home} />
+    <Route path="/home"  component={Home} />
     <Route path="/about"  component={About} />
+    <Route path="/services"  component={Services} />
+    <Route path="/contactus" component={ContactUs} />
+    <Route path="/signup"  component={SignUp} />
     </Switch>
     </Router>
   );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,23 +17,26 @@ const Navbar = () => {
         </NavLink>
         <Bars />
         <NavMenu>
+        <NavLink to='/home' activeStyle>
+           Home
+           </NavLink>
           <NavLink to='/about' activeStyle>
             About
           </NavLink>
           <NavLink to='/services' activeStyle>
             Services
           </NavLink>
-          <NavLink to='/contact-us' activeStyle>
+          <NavLink to='/contactus' activeStyle>
             Contact Us
           </NavLink>
-          <NavLink to='/sign-up' activeStyle>
+          <NavLink to='/signup' activeStyle>
             Sign Up
           </NavLink>
           {/* Second Nav */}
           {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
         </NavMenu>
         <NavBtn>
-          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          <NavBtnLink to='/signin'>Sign In </NavBtnLink>
         </NavBtn>
       </Nav>
     </>
